59d4b531f9ee7a6864c7c1a8f248c39921340879
Onome Akpobaro <onomeakpobaro@yahoo.com>
2025-07-03 12:51:13 +0100

    Added all project files

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./1-index.html",
+    "./2-index.html",
+    "./3-nav_index.html",
+
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
